a1f1b4bf4b32aa30a1f3c08138117378f4b073ba
Zeeshan Ahmed <zeeshan.ahmed@invozone.com>
2021-12-16 16:42:43 +0500

    Change the scope of fetchContacts method in OCKConstactsListViewController

--- OTFCareKit/OTFCareKit/iOS/Higher Order/ViewController/OCKContactsListViewController.swift
+++ OTFCareKit/OTFCareKit/iOS/Higher Order/ViewController/OCKContactsListViewController.swift
@@ -99,7 +99,7 @@ open class OCKContactsListViewController: OCKListViewController {
 
     /// `fetchContacts` asynchronously retrieves an array of contacts stored in a `Result`
     /// and makes corresponding `OCKDetailedContactViewController`s.
-    private func fetchContacts() {
+    open func fetchContacts() {
         storeManager.store.fetchAnyContacts(query: OCKContactQuery(for: Date()), callbackQueue: .main) { [weak self] result in
             guard let self = self else { return }
             switch result {
